44ff386a1bc7a96a5d743542900ab42ad70dcc7b
ciber <ciber@Desktop-Gaming>
2022-01-17 19:36:57 +0100

    Ejercicio 19 corregido el enunciado

--- Ejercicios 6 Quincena/Ej19_Aitor/src/ej19_aitor/Ej19_Aitor.java
+++ Ejercicios 6 Quincena/Ej19_Aitor/src/ej19_aitor/Ej19_Aitor.java
@@ -9,14 +9,16 @@ import java.util.*;
  *
  * @author Aitor
  * Necesitamos crear un programa para mostrar el ranking de puntuaciones de un torneo de ajedrez con 8 jugadores.
- * Se le pedirá al usuario que introduzca las puntuaciones de todos los jugadores (habitualmente valores entre 1000 y 2800, de tipo entero) y luego muestre las puntuaciones en orden descendente (de la más alta a la más baja).
+ * Se le pedirá al usuario que introduzca las puntuaciones de todos los jugadores
+ * (habitualmente valores entre 1000 y 2800, de tipo entero)
+ * y luego muestre las puntuaciones en orden descendente (de la más alta a la más baja).
  */
 public class Ej19_Aitor {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int array[] = new int[8];
-        int i = 0, puesto = 1;
+        int i, puesto = 1;
         
         System.out.println("Bienvenido a un programa para mostrar el ranking de puntuaciones de un torneo de ajedrez con 8 jugadores.");
         
