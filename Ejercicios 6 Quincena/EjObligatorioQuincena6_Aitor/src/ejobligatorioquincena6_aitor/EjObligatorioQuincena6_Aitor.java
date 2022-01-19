21b748dfc0319f86d02fe54542bb6a7fa7b887d9
Aitor <Aitor.gallardo.14@gmail.com>
2022-01-19 17:44:40 +0100

    Ejercicio obligatorio T6 WIP

--- Ejercicios 6 Quincena/EjObligatorioQuincena6_Aitor/src/ejobligatorioquincena6_aitor/EjObligatorioQuincena6_Aitor.java
+++ Ejercicios 6 Quincena/EjObligatorioQuincena6_Aitor/src/ejobligatorioquincena6_aitor/EjObligatorioQuincena6_Aitor.java
@@ -3,13 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejobligatorioquincena6_aitor;
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
 
 /**
  *
  * @author aitor
  */
 public class EjObligatorioQuincena6_Aitor {
+    
+    public static void entradaNombres(String[] nombres){
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 8; i++){
+            System.out.print("Introduzca el nombre de un estudiante: ");
+            nombres[i] = sc.nextLine();
+            
+        }
+    }
 
 
     public static void main(String[] args) {
@@ -19,20 +29,6 @@ public class EjObligatorioQuincena6_Aitor {
         System.out.println("Bienvenido");
         entradaNombres(nombres);
         
-        for (int i = 0; i < nombres.length; i++){
-            System.out.println(nombres[i]);
-        }
       
     }
-    
-    public static void entradaNombres(String[] nombres){
-        Scanner sc = new Scanner(System.in);
-        nombres = new String[8];
-        for (int i = 0; i < 8; i++){
-            System.out.print("Introduzca el nombre de un estudiante: ");
-            nombres[i] = sc.nextLine();
-            
-        }
-    }
-    
 }
